c2a34777662929175ebb59457b6478b605a38cb9
miquelg <miquelg@crumbit.org>
2020-10-02 12:38:47 +0200

    new (old) animation system

--- src/app/pages/sign/sign.component.ts
+++ src/app/pages/sign/sign.component.ts
@@ -1,86 +1,41 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {User} from '../../classes/User';
 import {UserInteractor} from '../../interactors/UserInteractor';
 import {Subscription} from 'rxjs';
-import {
-    trigger,
-    state,
-    style,
-    animate,
-    transition, stagger, query,
-} from '@angular/animations';
 
 @Component({
     selector: 'app-sign',
-    animations: [
-        trigger('leftBox', [
-            state('signIn', style({
-                width: '50%',
-                transform: 'rotate(0deg)'
-            })),
-            state('signUp', style({
-                width: '50%',
-                transform: 'rotate(0deg)'
-            })),
-            state('loading', style({
-                width: '100%',
-                transform: 'rotate(360deg)'
-            })),
-            transition('loading => signIn', [
-                animate('0.5s', style({
-                    width: '50%',
-                    transform: 'rotate(0deg)'
-                }))
-            ]),
-            transition('loading => signUp', [
-                animate('0.5s', style({
-                    width: '50%',
-                    transform: 'rotate(0deg)'
-                }))
-            ]),
-            transition('* => loading', [
-                animate('0.5s')
-            ])
-        ]),
-        trigger('rightBox', [
-            state('signIn', style({
-                transform: 'translateX(0)',
-                width: '50%',
-                opacity: 1
-            })),
-            state('signUp', style({
-                transform: 'translateX(0)',
-                width: '50%',
-                opacity: 1
-            })),
-            state('loading', style({
-                transform: 'translateX(200%)',
-                width: '0',
-                opacity: 0
-            })),
-            transition('loading => signIn', [
-                animate('0.5s')
-            ]),
-            transition('loading => signUp', [
-                animate('0.5s')
-            ]),
-            transition('* => loading', [
-                animate('0.5s')
-            ])
-        ])
-    ],
     templateUrl: './sign.component.html',
     styleUrls: ['./sign.component.scss']
 })
 export class SignComponent implements OnInit, OnDestroy {
 
     getCurrentUserSub: Subscription;
-    display: 'signIn' | 'signUp' | 'loading' = 'signIn';
+    display: 'signIn' | 'signUp' | 'preloading' | 'loading' | 'loaded' = 'signIn';
+    totalLoad: number = 0;
+    percentage: number = 0;
+    angle: number = 0;
 
     constructor(private userInteractor: UserInteractor) {
         this.getCurrentUserSub = this.userInteractor.getCurrentUserObs().subscribe((user: User) => {
             if (user !== null) {
-                this.display = 'loading';
+                this.preloadState();
+                setTimeout(() => {
+                    this.display = 'loading';
+                }, 600);
+                const circle = document.getElementById('circle');
+                setTimeout(() => {
+                    const interval = setInterval(() => {
+                        circle.setAttribute('stroke-dasharray', this.totalLoad + ', 20000');
+                        if (this.angle >= 360) {
+                            clearInterval(interval);
+                            this.loadedState();
+                        }
+                        this.percentage = Math.round(this.angle / 360 * 100);
+                        this.angle += 3;
+                        this.totalLoad += 13;
+                    }, 10);
+                }, 600);
             } else {
                 this.display = 'signIn';
             }
@@ -101,10 +56,34 @@ export class SignComponent implements OnInit, OnDestroy {
     }
 
     changeDisplay(display: any): void {
-        // this.display = 'loading';
-        // setTimeout(() => {
-            this.display = display;
-        // }, 500);
+        this.display = display;
     }
 
+    preloadState(): void {
+        this.display = 'preloading';
+        const logo = document.getElementsByClassName('sign-logo')[0] as HTMLDivElement;
+        const leftBox = document.getElementsByClassName('left-box')[0] as HTMLDivElement;
+        const rightBox = document.getElementsByClassName('right-box')[0] as HTMLDivElement;
+
+        logo.style.transform = 'rotate(360deg)';
+        logo.style.animation = 'rotateImage 500ms';
+        leftBox.style.width = '100%';
+        leftBox.style.animation = 'widthComplete 500ms';
+        rightBox.style.right = '-2000px';
+        rightBox.style.animation = 'hideRightBox 500ms';
+    }
+
+    loadedState(): void {
+        const logo = document.getElementsByClassName('sign-logo')[0] as HTMLDivElement;
+        const leftBox = document.getElementsByClassName('left-box')[0] as HTMLDivElement;
+
+        setTimeout(() => {
+            this.display = 'loaded';
+            leftBox.style.width = '0';
+            leftBox.style.left = '-2000px';
+            logo.style.transform = 'rotate(-360deg)';
+        }, 500);
+        logo.style.animation = 'rotateImageLeft 1000ms 500ms';
+        leftBox.style.animation = 'leftBoxLeaves 1000ms 500ms';
+    }
 }
