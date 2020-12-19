62b96049a63bb025de38fb949730e284654f6790
Yvazz <ziroustyle@gmail.com>
2020-12-19 18:06:37 +0100

    added image upload service and testing component

---
+++ src/app/services/image.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpService} from './http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+
+    constructor(private httpService: HttpService) {}
+
+
+    public uploadImage(image: File): Observable<Response> {
+        const formData = new FormData();
+
+        formData.append('image', image);
+
+        return this.httpService.post('/image-upload', formData);
+    }
+}
+
