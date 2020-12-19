1e2853d4601b89aed104552804a8ed56797a2679
Yvazz <ziroustyle@gmail.com>
2020-12-19 18:03:50 +0100

    added image upload service and testing component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {UserInteractor} from '../../interactors/UserInteractor';
 import {Router} from '@angular/router';
+import {ImageService} from '../../services/image.service';
 
+class ImageSnippet {
+    pending: boolean = false;
+    status: string = 'init';
+    constructor(public src: string, public file: File) {}
+}
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
@@ -9,8 +15,11 @@ import {Router} from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
+    selectedFile: ImageSnippet;
+
     constructor(private userInteractor: UserInteractor,
-                private router: Router) {
+                private router: Router,
+                private imageService: ImageService) {
     }
 
     ngOnInit(): void {
@@ -22,5 +31,40 @@ export class HomeComponent implements OnInit {
 
     play(): void {
         this.router.navigate(['./sign']);
+    }
+
+    uploadImageForm(): void {
+        console.log('deku');
+    }
+
+    private onSuccess(): void {
+        this.selectedFile.pending = false;
+        this.selectedFile.status = 'ok';
+    }
+
+    private onError(): void {
+        this.selectedFile.pending = false;
+        this.selectedFile.status = 'fail';
+        this.selectedFile.src = '';
+    }
+
+    processFile(imageInput: any): void {
+        const file: File = imageInput.files[0];
+        const reader = new FileReader();
+
+        reader.addEventListener('load', (event: any) => {
+
+            this.selectedFile = new ImageSnippet(event.target.result, file);
+
+            this.imageService.uploadImage(this.selectedFile.file).subscribe(
+                (res) => {
+                    this.onSuccess();
+                },
+                (err) => {
+                    this.onError();
+                });
+        });
+
+        reader.readAsDataURL(file);
     }
 }
