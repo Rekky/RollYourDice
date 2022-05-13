4d4ee514eaa9a168f1f357fdb918f4a3d7f6c10a
Luis <luisangel.contractor@bbva.com>
2022-05-13 02:41:06 +0200

    maqueta zoom 2

--- src/app/pages/game-editor/map-zoom-tools/map-zoom-tools.component.ts
+++ src/app/pages/game-editor/map-zoom-tools/map-zoom-tools.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-map-zoom-tools',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapZoomToolsComponent implements OnInit {
 
+  @Input() zoom: number = 0;
+  @Output() zoomChange: EventEmitter<number> = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setZoom(zoom: number):void {
+    this.zoom = zoom;
+    this.zoomChange.emit(this.zoom);
   }
 
 }
