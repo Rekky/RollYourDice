94ed267286324d8a25b4b6cbecf4cefbecf38db1
luis <luisangel.rivas@bbva.com>
2022-07-12 11:39:47 +0200

    fix zoom to 1 when new map created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {UserInteractor} from './interactors/UserInteractor';
 import {NavigationEnd, Router} from '@angular/router';
-import {SocketService} from './services/socket.service';
 
 @Component({
     selector: 'app-root',
@@ -11,7 +10,6 @@ import {SocketService} from './services/socket.service';
 export class AppComponent {
 
     constructor(private userInteractor: UserInteractor,
-                private socketService: SocketService,
                 private router: Router) {
         // routing events
         this.router.events.subscribe((ev: any) => {
