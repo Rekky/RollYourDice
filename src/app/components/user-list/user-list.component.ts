a21784162288acb3b5bbcfc651dcd2a6826b27c9
luis <ziroustyle@gmail.com>
2022-10-11 03:25:28 +0200

    temporal dom for modal see players

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -3,6 +3,7 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {Game} from '../../classes/Game';
 import {Player} from '../../classes/User';
 import {SocketService} from '../../services/socket.service';
+import {UntypedFormControl, UntypedFormGroup, Validators} from "@angular/forms";
 
 @Component({
     selector: 'app-user-list',
@@ -12,22 +13,24 @@ import {SocketService} from '../../services/socket.service';
 export class UserListComponent implements OnInit {
     players: Player[];
     playersRequest: Player[];
-    loaded: boolean = false;
-    playerNameOrId: string = '';
+    loading: boolean = false;
+    fgSearch: UntypedFormGroup;
 
     constructor(
         private dialogRef: MatDialogRef<any>,
         private ngZone: NgZone,
         private socketService: SocketService,
-        @Inject(MAT_DIALOG_DATA) public data: {players: Player[], playersRequest: Player[], gameId: string,gameName: string}
-    ) { }
+        @Inject(MAT_DIALOG_DATA) public data: {players: Player[], playersRequest: Player[], gameId: string, game: Game}
+    ) {
+        this.fgSearch = new UntypedFormGroup({
+            search: new UntypedFormControl(null, Validators.required)
+        });
+    }
 
     ngOnInit(): void {
         this.players = this.data.players;
         this.playersRequest = this.data.playersRequest;
-        setTimeout(() => {
-            this.loaded = true;
-        }, 1000);
+        console.log('data', this.data);
     }
 
     closeDialog(): void {
